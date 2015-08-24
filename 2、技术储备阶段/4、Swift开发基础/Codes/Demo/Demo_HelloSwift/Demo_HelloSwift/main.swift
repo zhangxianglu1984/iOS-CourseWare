9491779bf646f7d51ea028c77a89d114c3228a81
zhangxianglu1984 <zhangxianglu1984@gmail.com>
2015-08-24 09:15:06 +0800

    增加了通知和UIPickerView

---
+++ 2、技术储备阶段/4、Swift开发基础/Codes/Demo/Demo_HelloSwift/Demo_HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Demo_HelloSwift
+//
+//  Created by zhang xianglu on 15/8/16.
+//  Copyright (c) 2015年 zhang xianglu. All rights reserved.
+//
+
+//import Foundation
+
+println("Hello, World!")
+
